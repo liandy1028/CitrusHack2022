6fd930e6d4ff743ed6891bd6914f015c90e0feac
Andy Li <liandy1028@gmail.com>
2022-04-02 13:36:20 -0700

    basic template for bot

--- bot.py
+++ bot.py
@@ -1,3 +1,15 @@
+import os
 import discord
+from discord.ext import commands
 import googletrans
+from dotenv import load_dotevn
 
+load_dotevn()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot.run(TOKEN)
