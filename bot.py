380c569f39ea6088766a5f36e32d2a284feeb540
Andy Li <liandy1028@gmail.com>
2022-04-02 13:50:06 -0700

    created commands

--- bot.py
+++ bot.py
@@ -2,14 +2,32 @@ import os
 import discord
 from discord.ext import commands
 import googletrans
-from dotenv import load_dotevn
+from dotenv import load_dotenv
 
-load_dotevn()
+load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ONLINE")
+
+@bot.command(name='help')
+async def help(ctx):
+    pass
+
+@bot.command(name='start')
+async def start(ctx):
+    pass
+
+@bot.command(name='translate')
+async def translate(ctx):
+    pass
+
+
 
 bot.run(TOKEN)
